8010cb67c6ee9036b2602045abccce06baa3107b
Vlad Soroka <toolbox@provider>
2024-07-14 17:31:38 +0300

    adding point override to smartStack

--- Sources/Toolbox/extensions/udf/SmartStackView.swift
+++ Sources/Toolbox/extensions/udf/SmartStackView.swift
@@ -218,6 +218,19 @@ public class SmartStackView: UIStackView, StackableView {
         endEditing(true)
     }
     
+    public override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        for subview in subviews as [UIView] {
+            if !subview.isHidden
+                && subview.alpha > 0
+                && subview.isUserInteractionEnabled
+                && subview.point(inside: convert(point, to: subview), with: event) {
+                return true
+            }
+            
+        }
+        return false
+    }
+    
 }
 
 extension SmartStackView.Props: StackableProp {
